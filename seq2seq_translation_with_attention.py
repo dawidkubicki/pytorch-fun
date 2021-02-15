8dc0fee12e30eafcca035adf9b4a02e94b4d3d50
Dawid <dawid@pluscode.io>
2021-02-15 19:33:35 +0100

    pytorch seq2seq with attention using one-hot encoder

---
+++ seq2seq_translation_with_attention.py
@@ -0,0 +1,31 @@
+# In this project we will to a neural translation with seq2seq architecture using attention mechanism
+
+from __future__ import unicode_literals, print_function, division
+from io import open
+import unicodedata
+import string
+import re
+import random
+
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+SOS_token = 0
+EOS_token = 1
+
+class Lang:
+	def __init__(self, name):
+		self.name = name
+		self.word2index = {}
+		self.word2count = {}
+		self.index2word = {0: "SOS", 1: "EOS"}
+		self.n_words = 2 # count sos and eos
+
+
+	def addSentence(se)
+
+
